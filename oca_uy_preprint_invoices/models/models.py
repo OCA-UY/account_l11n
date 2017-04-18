3cfeb69f4786b1adf4e73879fac53368cf5ddd78
Santiago Acosta y Lara <sacostaylara@tdtconsultants.com>
2017-04-18 20:09:37 -0300

    template para impreso, se implementa funcion en account_invoice para calcular monto por impuesto

--- oca_uy_preprint_invoices/models/models.py
+++ oca_uy_preprint_invoices/models/models.py
@@ -13,3 +13,23 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def _get_tax_amount_by_tax(self):
+        self.ensure_one()
+        res = {}
+        res.setdefault(self.env['account.tax'].search([('name', 'ilike', '%Ventas (10%')]), [0.0, 0.0])
+        res.setdefault(self.env['account.tax'].search([('name', 'ilike', '%Ventas (22%')]), [0.0, 0.0])
+        res.setdefault(self.env['account.tax'].search([('name', 'ilike', '%Ventas Exentos%')]), [0.0, 0.0])
+        currency = self.currency_id or self.company_id.currency_id
+        for line in self.tax_line_ids:
+            res.setdefault(line.tax_id, [0.0, 0.0])
+            res[line.tax_id][0] += line.base
+            res[line.tax_id][1] += line.amount
+        res = sorted(res.items(), key=lambda l: l[0].sequence)
+        res = map(lambda l: (l[1][0], l[1][1]), res)
+        return res
